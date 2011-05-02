78847ad5db05b213224588a7c495c34795abd1f3
Simon Murtha-Smith <simon@murtha-smith.com>
2011-05-02 12:22:45 -0700

    Basic contactd locker app working to listen for contact/twitter events

---
+++ Apps/contactd/contactd.app
@@ -0,0 +1,7 @@
+{
+    "title":"Contact Listener",
+    "action":"hangin out, listenin, pushing a bit",
+    "desc":"listenin for contact updates and pushin em to the ContactInfo App",
+    "status":"unstable",
+    "run":"node contactd.js"
+}
---
+++ Apps/contactd/contactd.js
@@ -0,0 +1,42 @@
+/*
+*
+* Copyright (C) 2011, The Locker Project
+* All rights reserved.
+*
+* Please see the LICENSE file for more information.
+*
+*/
+
+var express = require('express'),
+    connect = require('connect'),
+    fs = require('fs');
+
+var locker;
+
+var app = express.createServer(connect.bodyParser());
+
+app.get('/', function(req, res) {
+    res.writeHead(200);
+    res.end('yeah!!');
+});
+
+app.post('/contact', function(req, res) {
+    res.writeHead(200);
+    var contact = req.body;
+    console.log('contact:', contact);
+    res.end('1');
+});
+
+var stdin = process.openStdin();
+stdin.setEncoding('utf8');
+stdin.on('data', function (chunk) {
+    var processInfo = JSON.parse(chunk);
+    process.chdir(processInfo.workingDirectory);
+    console.error(process.cwd());
+    locker = require(process.cwd() + '/../../Common/node/locker.js');
+    locker.initClient(processInfo);
+    app.listen(processInfo.port);
+    var returnedInfo = {port: processInfo.port};
+    console.log(JSON.stringify(returnedInfo));
+    locker.listen('contact/twitter', '/contact');
+});
