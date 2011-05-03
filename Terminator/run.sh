0e6298f808376d6a720e9346c1b547c708158221
jer <jer@six.(none)>
2011-05-03 16:32:41 -0500

    batch

--- Terminator/run.sh
+++ Terminator/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+git pull origin master
+
+# add new TOS to track here, just cut/paste the same pattern
 lynx -dump -useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13" http://www.facebook.com/terms.php > facebook_terms.txt
 lynx -dump -useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13" http://developers.facebook.com/policy/ > facebook_dev_policy.txt
+
 git commit -am "batch"
 git push origin master
 
